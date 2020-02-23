8ed53d304fbc3e4fde836282e09be54d61873c2a
Kevin Loughead <kvnloughead@gmail.com>
2020-02-23 16:38:08 -0500

    "Starting RESTful blog project.  Implemented basic INDEX route."

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express"),
+      bodyParser = require("body-parser"),
+      mongoose = require("mongoose"),
+      request = require("request"),
+      app = express()
+
+// App config
+mongoose.connect("mongodb://localhost:27017/blog", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+// Mongoose config
+var blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test",
+//     image: "https://images.unsplash.com/photo-1484100356142-db6ab6244067?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//     body: "Sparkle sparkle..."
+// });
+
+// Routes
+
+app.get("/", function(req, res){
+    res.redirect("/blogs")
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            // TODO - better error handling
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Serving Blog at port 3000...");
+});
+
